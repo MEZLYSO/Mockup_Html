ead0863d95347aea89d649c58029bcbd6168baaf
Maury <lynuxazura@gmail.com>
2025-02-24 15:01:10 -0600

    New 3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const elements = ["banner","sec1","sec2","sec3"]
 function actualizarBanner(container) {
     const banner = document.getElementById(`${container}`);
-    banner.style.height = `${window.innerHeight}px`;
+    banner.style.height = `${window.innerHeight-100}px`;
 }
 
 elements.forEach((div)=>{
