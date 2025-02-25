b0521e3226bd220e4712f5d58c16a67adafb5065
Maury <lynuxazura@gmail.com>
2025-02-25 12:45:58 -0600

    Ahora si ya creo

--- js/main.js
+++ js/main.js
@@ -3,14 +3,11 @@ function actualizarBanner(container) {
     const banner = document.getElementById(`${container}`);
     banner.style.height = `${window.innerHeight-100}px`;
 }
-
 elements.forEach((div)=>{
     actualizarBanner(`${div}`);
     window.addEventListener('resize',actualizarBanner);
 })
-
 const button = document.getElementById("btn")
-
 button.addEventListener("click",()=>{
     location.href = "index2.html"
 })
